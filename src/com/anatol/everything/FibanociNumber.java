ecd4f024b46a8ef705c007a344afa468e2621ea8
cameltoe13 <anatolschiopu0+3@gmail.com>
2019-06-10 01:38:33 +0300

    fibanoci 2

--- src/com/anatol/everything/FibanociNumber.java
+++ src/com/anatol/everything/FibanociNumber.java
@@ -2,4 +2,14 @@ package com.anatol.everything;
 
 public class FibanociNumber {
 
+    static int fibanoci(int n){
+        if (n <= 1)
+            return n;
+        return fibanoci(n-1) + (n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 9;
+        System.out.println(fibanoci(n));
+    }
 }
