dc1073ec8c4ef580a2858b607cd8ee0727ec9b6b
cameltoe13 <anatolschiopu0+3@gmail.com>
2019-06-18 13:51:42 +0300

    commit 18.06.2019

--- src/com/anatol/serialization/and/deserialization/Main.java
+++ src/com/anatol/serialization/and/deserialization/Main.java
@@ -1,5 +1,40 @@
 package com.anatol.serialization.and.deserialization;
 
+import java.io.*;
+
 public class Main {
 
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Anatol", 24, "Male");
+        Person person2 = new Person("Nicolae", 23, "Male");
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(new File("file1.txt"));
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+            objectOutputStream.writeObject(person1);
+            objectOutputStream.writeObject(person2);
+
+            objectOutputStream.close();
+            fileOutputStream.close();
+
+            FileInputStream fileInputStream = new FileInputStream(new File("file1.txt"));
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+            Person pr1 = (Person) objectInputStream.readObject();
+            Person pr2 = (Person) objectInputStream.readObject();
+
+            System.out.println(pr1.toString());
+            System.out.println(pr2.toString());
+
+            objectInputStream.close();
+            fileInputStream.close();
+
+        } catch (IOException e){
+            System.out.println("Error in initializing stream");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
 }
