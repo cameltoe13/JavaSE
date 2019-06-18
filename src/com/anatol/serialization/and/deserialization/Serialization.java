78f489fd4ad6f8c18e64822473f0b3cc74b488bc
cameltoe13 <anatolschiopu0+3@gmail.com>
2019-06-18 14:16:56 +0300

    change

--- src/com/anatol/serialization/and/deserialization/Serialization.java
+++ src/com/anatol/serialization/and/deserialization/Serialization.java
@@ -2,12 +2,13 @@ package com.anatol.serialization.and.deserialization;
 
 import java.io.*;
 
-public class Main {
+public class Serialization {
 
     public static void main(String[] args) {
 
         Person person1 = new Person("Anatol", 24, "Male");
         Person person2 = new Person("Nicolae", 23, "Male");
+        Person person3 = new Person("Dinu", 23, "Male");
 
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(new File("file1.txt"));
@@ -15,26 +16,27 @@ public class Main {
 
             objectOutputStream.writeObject(person1);
             objectOutputStream.writeObject(person2);
+            objectOutputStream.writeObject(person3);
 
             objectOutputStream.close();
             fileOutputStream.close();
 
-            FileInputStream fileInputStream = new FileInputStream(new File("file1.txt"));
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            System.out.println("Objects was serialized");
 
-            Person pr1 = (Person) objectInputStream.readObject();
-            Person pr2 = (Person) objectInputStream.readObject();
-
-            System.out.println(pr1.toString());
-            System.out.println(pr2.toString());
-
-            objectInputStream.close();
-            fileInputStream.close();
+//            FileInputStream fileInputStream = new FileInputStream(new File("file1.txt"));
+//            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+//
+//            Person pr1 = (Person) objectInputStream.readObject();
+//            Person pr2 = (Person) objectInputStream.readObject();
+//
+//            System.out.println(pr1.toString());
+//            System.out.println(pr2.toString());
+//
+//            objectInputStream.close();
+//            fileInputStream.close();
 
         } catch (IOException e){
             System.out.println("Error in initializing stream");
-        } catch (ClassNotFoundException e){
-            e.printStackTrace();
         }
     }
 }
