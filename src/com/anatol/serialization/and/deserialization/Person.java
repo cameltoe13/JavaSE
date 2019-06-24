8a85cfdd9a48b010f3d44665a2d5cb4ee7f1762f
cameltoe13 <anatolschiopu0+3@gmail.com>
2019-06-24 10:39:16 +0300

    person change

--- src/com/anatol/serialization/and/deserialization/Person.java
+++ src/com/anatol/serialization/and/deserialization/Person.java
@@ -1,6 +1,7 @@
 package com.anatol.serialization.and.deserialization;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Person implements Serializable {
 
@@ -13,6 +14,21 @@ public class Person implements Serializable {
         this.name = name;
         this.age = age;
         this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                Objects.equals(name, person.name) &&
+                Objects.equals(gender, person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, gender);
     }
 
     @Override
