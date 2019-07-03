815c33779cc29ef6787590ff1948f76daded2369
cameltoe13 <anatolschiopu0+3@gmail.com>
2019-07-03 20:58:11 +0300

    binary search

--- src/com/anatol/everything/BinarySearch.java
+++ src/com/anatol/everything/BinarySearch.java
@@ -6,13 +6,13 @@ public class BinarySearch {
 
         if (start < end) {
             int mid = start + (end - start) / 2;
-            if (key < sortedArray[mid]) {
-                return recursiveBinarySearch(sortedArray, start, mid, key);
 
+            if (key < sortedArray[mid]) {
+                return recursiveBinarySearch(sortedArray, start, mid -1, key);
             } else if (key > sortedArray[mid]) {
                 return recursiveBinarySearch(sortedArray, mid+1, end , key);
 
-            } else {
+            } else if(key == sortedArray[mid]){
                 return mid;
             }
         }
@@ -24,12 +24,12 @@ public class BinarySearch {
         int[] arr1 = {0, -5, 2, 45, 234, 567, 876, 900, 976, 999};
 
         int index = recursiveBinarySearch(arr1,0,arr1.length,0);
-        System.out.println("Found 45 at "+index+" index");
+        System.out.println("Found 0 at "+index+" index");
 
         index = recursiveBinarySearch(arr1,0,arr1.length,45);
         System.out.println("Found 45 at "+index+" index");
 
-        index = recursiveBinarySearch(arr1,0,arr1.length,999);
+        index = recursiveBinarySearch(arr1, 0,arr1.length,999);
         System.out.println("Found 999 at "+index+" index");
 
         index = recursiveBinarySearch(arr1,0,arr1.length,876);
